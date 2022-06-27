aa97394ac0b10c4f5172cfc3e7068fcbbed1368d
morimi <mori_misaki@qualiarts.jp>
2022-06-27 19:02:20 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const visit = require(`unist-util-visit`)
+
+module.exports = ({ markdownAST }) => {
+  const htmls = []
+
+  visit(markdownAST, "html", node => {
+    htmls.push(node)
+
+    if (node.value.indexOf("<img") >= 0 ) {
+
+      if (node.value.indexOf('.gif') >= 0) { // GIF image
+        if (/<img.*class=/.test(node.value)) {
+          node.value = node.value.replaceAll(/class="([\w\d-_\s]+)"/g, 'class="$1 lazyload"')
+        } else {
+          node.value = node.value.replaceAll(
+            "src=",
+            'class="lazyload" src='
+          )
+        }
+      } else {
+        node.value = node.value.replaceAll(
+          "gatsby-resp-image-image",
+          "gatsby-resp-image-image lazyload"
+        )
+      }
+      
+      node.value = node.value.replaceAll("src=", "data-src=")
+      node.value = node.value.replaceAll("srcset=", "data-srcset=")
+    }
+  })
+  
+  return markdownAST
+}
